813d78ee735a167e5582befbfabb7f128d0f050e
James Chaszar <jchaszar@email.sc.edu>
2018-11-06 23:15:25 -0500

    added base.html and Post Model

--- rank/views.py
+++ rank/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Jchaszar',
-        'title': 'Rank posting 1',
-        'content': '...',
-        'date_posted': 'November 6, 2018'
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Rank posting 2',
-        'content': '...222',
-        'date_posted': 'November 6, 2018'
-    }
 
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'rank/home.html', context)
 
